c5c12e053a88edc6be0ba3b25a5d1b26e2cf5d4f
Martin Aguero <martin.aguero84@gmail.com>
2024-03-16 21:57:50 -0500

    worked on tag-routes.js; added its get by id, post, put to update by id, and delete by id routes. Tested and working well.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -15,18 +15,48 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  Tag.findByPk(req.params.id)
+  .then((tag) => {
+    res.json(tag);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+  .then((tagCreated) => {
+    res.json(tagCreated);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update(
+  {
+    tag_name: req.body.tag_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagUpdated) => {
+    res.json(tagUpdated);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((tagDeleted) => {
+    res.json(tagDeleted);
+  });
 });
 
 module.exports = router;
