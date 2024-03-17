fa43d6ebbf48e435c2b3c71fc27063f9bd71d6b7
Martin Aguero <martin.aguero84@gmail.com>
2024-03-16 21:03:59 -0500

    added to the category routes, added the get by id params, the post, the put, and the delete routes. Tested and all works well for the category-routes.js.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { where } = require('sequelize');
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
@@ -15,18 +16,52 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((category) => {
+    res.json(category);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then((categoryUpdated) => {
+    res.json(categoryUpdated);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+  {
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+    id: req.params.id
+    }
+  })
+  .then((categoryUpdated) => {
+    res.json(categoryUpdated);
+  });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((categoryDeleted) => {
+    res.json(categoryDeleted);
+  });
 });
 
 module.exports = router;
